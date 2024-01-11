3a351ab003a8b40783c8d6e9d8e1a17f1b519c08
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-01-11 19:52:56 +0530

    style:added style in task

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const addTodoToList = document.getElementById("todo-data");
 
 function addToDo(todo) {
+    //creating div element
   const addRow = document.createElement("div");
   const addItemList = document.createElement("div");
 
@@ -12,12 +13,24 @@ function addToDo(todo) {
   const finishedButton = document.createElement("button");
   const createhr = document.createElement("hr");
 
+  //adding style
+  addItemList.classList.add("todo-item" ,"d-flex" ,"flex-row", "justify-content-between", "align-items-center")
+  srNo.classList.add('todo-no')
+  taskTodo.classList.add("todo-detail", "text-muted")
+  status.classList.add("todo-status", "text-muted")
+  toDoButtonGroup.classList.add("todo-actions","d-flex", "justify-content-start", "gap-2")
+  finishedButton.classList.add("btn", "btn-success")
+  deleteButton.classList.add("btn", "btn-danger")
+  
+
+  // adding content to that div
   deleteButton.textContent = "Delete";
   finishedButton.textContent = "finished";
   srNo.textContent = "1";
   taskTodo.textContent = todo;
   status.textContent = "pending";
 
+  //appending child
   addItemList.appendChild(srNo);
   addItemList.appendChild(taskTodo);
   addItemList.appendChild(status);
